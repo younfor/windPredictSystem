52f0899dd1752e4c36ba2986b63ab8043fdfddf8
younfor <younfor@yeah.net>
2015-01-15 15:52:17 +0800

    add maps

--- mysite/wind/urls.py
+++ mysite/wind/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns= patterns('wind.views',
             url(r'^login/','login',name='login'),
             url(r'^index/','index',name='index'),
+            url(r'^portal/','portal',name='portal'),
             )
