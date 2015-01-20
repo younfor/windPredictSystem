11a54bc739f2a0707af3651ed4e3571a2d55d00f
younfor <younfor@yeah.net>
2015-01-20 13:38:43 +0800

    add leaflet.js - search control

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^wind/', include('wind.urls'),name='wind'),
     url(r'^admin/', include(admin.site.urls),name='admin'),
+
 )
