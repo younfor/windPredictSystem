1f6101943c980075e02afdef571b31f1e74748a5
shinne <675519530@qq.com>
2015-01-21 15:55:04 +0800

    login&signup

--- mysite/wind/views.py
+++ mysite/wind/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render,render_to_response,HttpResponseRedirect
 from django.template import RequestContext   
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import auth
 from wind.models import UserProfile
@@ -18,16 +18,21 @@ def  index(request):
 login 
 '''
 def  login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user=auth.authenticate(username=username,password=password)
-    if user is not None:
-        if user.is_active:
-        	auth.login(request,user)
-        	context={'result':'login success!'}
-    else:
-        context={'result':'login failed!'}
-    return render_to_response('wind/index.html',context,context_instance=RequestContext(request) )
+
+    if request.method == 'POST': 
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        #print username,password
+       # isuser=User.objects.filter(username=username,password=password)
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            print user.username,user.password
+            request.session['username']=username
+            response=HttpResponseRedirect('../portal')
+            #response.set_cookie('username',username,3600)
+            return response
+    return render(request,'login.html')
+
 
 
 
@@ -55,3 +60,19 @@ def chartout(request):
    #print "Matrix values: ", data[:]
     return render_to_response('chartout.html',{'week':week,'temper':temper}) 
 
+def signup(request):
+    if request.method == 'POST': 
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email=request.POST.get('email')
+        print username,password
+        user=User.objects.create_user(username,email,password)
+        user.is_staff=True
+        user.save()
+        print "%s,%s,%d"  % (user.username,user.password,user.is_staff)
+        #if form.is_valid(): 
+          #  f=form.cleaned_data
+            #info=f['username']
+            #print info
+        return HttpResponse('sucessful signup') 
+    return render(request,'signup.html')
