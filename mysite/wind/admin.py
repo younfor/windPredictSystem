d0a72e6b57aff17670f06d871e36ab8f3875ea32
younfor <younfor@yeah.net>
2015-03-11 16:21:45 +0800

    增加只读权限

--- mysite/wind/admin.py
+++ mysite/wind/admin.py
@@ -2,12 +2,35 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth import forms
+from django.contrib.auth import get_permission_codename
 from django.contrib.auth.models import User
 from wind.models import UserProfile
 from wind import models
 from django.utils.translation import ugettext, ugettext_lazy as _
 # which acts a bit like a singleton
 from django.contrib.auth import forms
+from django.contrib.admin.util import flatten_fieldsets
+
+
+class ReadOnlyAdmin(admin.ModelAdmin):
+
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.model._meta.get_all_field_names()
+
+    def has_add_permission(self, request):
+        # Nobody is allowed to add
+        if request.user.userprofile.level == 1:
+            return False
+        else:
+            return super(ReadOnlyAdmin, self).has_add_permission(request)
+
+    def has_delete_permission(self, request, obj=None):
+        # Nobody is allowed to delete
+        if request.user.userprofile.level == 1:
+            return False
+        else:
+            return super(ReadOnlyAdmin, self).has_delete_permission(request)
 
 
 class UserProfileInline(admin.StackedInline):
@@ -16,6 +39,10 @@ class UserProfileInline(admin.StackedInline):
     verbose_name_plural = 'UserProfile'
 
     def get_fields(self, request, obj=None):
+        # readonly for system_user
+        if request.user.userprofile.level == '1':
+            self.readonly_fields = ['level', 'address', 'telephone', 'father']
+        # form
         form = super(UserProfileInline, self).get_formset(
             request, obj, fields=None).form
         # if 'level' in form.base_fields:
@@ -27,28 +54,30 @@ class UserProfileInline(admin.StackedInline):
 # Define a new User admin
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(UserAdmin, ReadOnlyAdmin):
     inlines = (UserProfileInline, )
-    list_display = ('username', 'is_staff', 'get_UserProfile')
+    list_display = ('username', 'is_staff', 'level')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_superuser',
+                                       'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
 
-    def get_UserProfile(self,obj):
-        return obj.userprofile
+    def level(self, obj):
+        return obj.userprofile.level
 
     def get_form(self, request, obj=None, **kwargs):
         # Get form from original UserAdmin.
         form = super(UserAdmin, self).get_form(request, obj, **kwargs)
         # here is according to the system
+        '''
         if 'user_permissions' in form.base_fields:
             permissions = form.base_fields['user_permissions']
             permissions.queryset = permissions.queryset.filter(
                 content_type__name='log entry')
+        '''
         return form
 
     def save_model(self, request, obj, form, change):
@@ -66,14 +95,14 @@ class UserAdmin(UserAdmin):
     def get_queryset(self, request):
         qs = super(UserAdmin, self).get_queryset(request)
         print 'ps:get_queryset'  # If super-user, show all
-        if request.user.is_superuser:
+        if request.user.is_superuser or (request.user.userprofile.level == '1'):
             return qs
         return qs.filter(userprofile__father=request.user.userprofile)
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 # register Models
-admin.site.register(models.PowerStation)
-admin.site.register(models.Factory)
-admin.site.register(models.WindTurbine)
-admin.site.register(models.PowerData)
+admin.site.register(models.PowerStation, ReadOnlyAdmin)
+admin.site.register(models.Factory, ReadOnlyAdmin)
+admin.site.register(models.WindTurbine, ReadOnlyAdmin)
+admin.site.register(models.PowerData, ReadOnlyAdmin)
