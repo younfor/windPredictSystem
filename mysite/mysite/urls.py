f955a179970f47fb871ff1c064443acc739dae7f
younfor <younfor@yeah.net>
2015-10-08 10:05:08 +0800

    fix img path

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^blog/', include('blog.urls')),    /E/dwen/model/output/original
+    url(r'^img/(?P<path>.*)', 'django.views.static.serve', {'document_root': '/E/dwen/model/output/original'}),  
     #url(r'^img/(?P<path>.*)', 'django.views.static.serve', {'document_root': '/home/shinneyou/model/output/original/2013_01_30min_gn'}),  
     url(r'^wind/', include('wind.urls'),name='wind'),
     url(r'^admin/', include(admin.site.urls),name='admin'),
